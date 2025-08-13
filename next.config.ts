26a8c9b49697c2133a46887af4b63e25798a1f53
Tobias Kremer <tobias.kremer@dg-i.net>
2025-08-13 17:15:30 +0200

    add dynamics

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
