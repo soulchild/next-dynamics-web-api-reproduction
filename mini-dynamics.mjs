ee8c06ef46ef307039a158ab35fb326b86425e09
Tobias Kremer <tobias.kremer@dg-i.net>
2025-09-18 09:21:51 +0200

    refactor: trim down even further

--- mini-dynamics.mjs
+++ mini-dynamics.mjs
@@ -1,8 +1,6 @@
 export default () => {
   // Removing the following condition fixes the problem
   if (true) {
-    let url = "api";
-    url += `/`; // Not appending here fixes the problem
-    return new URL(url, "https://example.com").toString();
+    return new URL("api/", "https://example.com").toString();
   }
 };
