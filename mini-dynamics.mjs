f4599684a82274e4bfdfa48f7f393d56d19b0fb0
Tobias Kremer <tobias.kremer@dg-i.net>
2025-09-18 09:41:55 +0200

    refactor: remove template literal

--- mini-dynamics.mjs
+++ mini-dynamics.mjs
@@ -2,7 +2,7 @@ export default () => {
   // Removing the following condition fixes the problem
   if (true) {
     let url = "api";
-    url += `/`; // Not appending here fixes the problem
+    url += "/"; // Not appending here fixes the problem
     return new URL(url, "https://example.com").toString();
   }
 };
