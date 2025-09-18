710f5be51707782123befe95d95027b80728cdca
Tobias Kremer <tobias.kremer@dg-i.net>
2025-09-18 09:03:06 +0200

    refactor: trim down even further

--- mini-dynamics.mjs
+++ mini-dynamics.mjs
@@ -1,30 +1,8 @@
-var getApiUrl = (serverUrl, apiConfig) => {
-  // Removing this condition fixes the problem
+export default () => {
+  // Removing the following condition fixes the problem
   if (true) {
     let url = "api";
-    if (apiConfig.path) {
-      url += `/${apiConfig.path}`;
-    }
-    return new URL(url, serverUrl).toString() + "/";
+    url += `/`; // Not appending here fixes the problem
+    return new URL(url, "https://example.com").toString();
   }
 };
-
-function mergeConfig(internalConfig, config) {
-  internalConfig.dataApi.url = getApiUrl(
-    config.serverUrl,
-    internalConfig.dataApi
-  );
-  return internalConfig;
-}
-
-export default (config) =>
-  mergeConfig(
-    {
-      serverUrl: null,
-      dataApi: {
-        path: "data",
-        url: "",
-      },
-    },
-    config
-  );
--- next-15.4.6/src/app/page.tsx
+++ next-15.4.6/src/app/page.tsx
@@ -1,10 +1,6 @@
 import assert from "node:assert";
 import miniDynamics from "../../../mini-dynamics.mjs";
 
-assert(
-  miniDynamics({
-    serverUrl: "https://test.api.crm4.dynamics.com",
-  }).dataApi.url === "https://test.api.crm4.dynamics.com/api/data/"
-);
+assert(miniDynamics() === "https://example/api/");
 
 export default async function Home() {}
